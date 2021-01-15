6d6ef25d4d23fde531afdad90aca5686807f85cd
Andrey Morozov <morozov@joom.com>
2021-01-15 15:52:09 +0300

    Use Logger framework. Add gitsubmodules

--- MagicKit/MagicKit/Wizard.m
+++ MagicKit/MagicKit/Wizard.m
@@ -6,15 +6,30 @@
 //
 
 #import "Wizard.h"
+@import Logger;
+
+@interface Wizard ()
+
+@property (nonatomic, strong, readonly) MessageLogger *logger;
+
+@end
 
 @implementation Wizard
 
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _logger = [MessageLogger new];
+    }
+    return self;
+}
+
 - (void)foo {
-    // need log
+    [self.logger logMessage:@"foo ✨"];
 }
 
 - (void)bar {
-    
+    [self.logger logMessage:@"bar 🤖"];
 }
 
 @end
